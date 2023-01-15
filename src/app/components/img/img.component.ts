b45b1851f6947012cfed8f71bd3c86f79a0aa203
Francisco <jhersonhuacho@gmail.com>
2023-01-15 07:39:27 -0500

    fin de componentes

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -9,6 +9,7 @@ export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy
 
   img: string = '';
   //@Input() img = '';
+  // eslint-disable-next-line @angular-eslint/no-input-rename
   @Input('img')
   set changeImg(newImg: string) {
     this.img = newImg;
