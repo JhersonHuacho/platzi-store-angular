4b326b70f39121a9ae7247052800382e3122266b
Francisco <jhersonhuacho@gmail.com>
2023-02-10 20:43:29 -0500

    creando un shared module

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -10,29 +10,18 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { ImgComponent } from './components/img/img.component';
-import { FundamentosComponent } from './components/fundamentos/fundamentos.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
-import { NavComponent } from './components/nav/nav.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
 
-import { WebsiteRoutingModule } from './website-routing.module';
+import { FundamentosComponent } from './components/fundamentos/fundamentos.component';
+import { NavComponent } from './components/nav/nav.component';
 import { SwiperModule } from 'swiper/angular'
+import { WebsiteRoutingModule } from './website-routing.module';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    ImgComponent,
     FundamentosComponent,
-    ProductComponent,
-    ProductsComponent,
     NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
     HomeComponent,
     CategoryComponent,
     ProductDetailComponent,
@@ -47,8 +36,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     WebsiteRoutingModule,
+    FormsModule,
     SwiperModule,
-    FormsModule
+    SharedModule
   ]
 })
 export class WebsiteModule { }
